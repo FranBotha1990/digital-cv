fff3c68ad6d69b06e7d3424da0de989497e102f5
FranBotha1990 <f.botha@outlook.com>
2024-07-28 23:23:14 +0300

    Updates

--- _nuxt/CLbSKh41.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,x as o,v as t,y as c}from"./D_UXvj2R.js";const n={},s=c("h1",null,"Hello world!",-1),r=[s];function _(a,l){return t(),o("div",null,r)}const i=e(n,[["render",_]]);export{i as default};
