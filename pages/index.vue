5877d6c3849b9dc5ec8c5cbed0e2dffa263afa46
FranBotha1990 <f.botha@outlook.com>
2024-07-29 22:47:20 +0300

    Improved Structure, Move Profile Summary to Right, Base Profile Content, CSS Classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,83 @@
 <template>
     <!-- Screen container, page-level -->
-    <main>
+    <main class="screen-cnt">
         <!-- Content container -->
-        <div>
+        <div class="content-cnt">
             <!-- CV container -->
-            <article>
-                <!-- Flex container for the columns -->
-                <div>
+            <article class="cv-cnt">
+                <!-- Column flex container for the -->
+                <div class="main-column-cnt">
+
                     <!-- Left column -->
-                    <aside>
+                    <aside class="left-column-cnt">
+
                         <!-- Profile picture -->
-                        <header></header>
+                        <header class="mb-6">
+                            <img src="/assets/img/profile.png" alt="Francois Botha" class="profile-pic">
+                        </header>
+
                         <!-- Contact information  -->
-                        <section></section>
-                        <!-- Profile summary -->
-                        <section></section>
+                        <section class="mb-6">
+                            <h1>Francois Botha</h1>
+                            <h2>Head of Business Information</h2>
+                            <h3>Contact Information</h3>
+                            <ul>
+                                <li><a href="">Turku, Finland</a></li>
+                                <li><a href="">f.botha@outlook.com</a></li>
+                                <li><a href="">LinkedIn</a></li>
+                                <li><a href="">Digital CV</a></li>
+                                <li><a href="">GitHub</a></li>
+                            </ul>
+                        </section>
+                        
                         <!-- Strengths section -->
-                        <section></section>
+                        <section class="mb-6"></section>
+                        
                         <!-- Skills section -->
-                        <section></section>
+                        <section class="mb-6"></section>
+                    
                     </aside>
 
                     <!-- Right column -->
-                    <section>
+                    <section class="right-column-cnt">
+
+                        <!-- Professional titles -->
+                        <section class="mb-6"></section>
+
+                        <!-- Profile summary -->
+                        <section class="mb-6">At Synexa Life Sciences, I've led the transformation of our IT infrastructure to meet stringent industry standards, ensuring robust, scalable, and innovative technology systems across five global sites. Leveraging my extensive background in IT operations and commitment to compliance, I've enhanced network reliability, fortified laboratory environments, and driven digital transformation initiatives that position Synexa at the forefront of biomarker and bioanalytical services technology. By implementing a comprehensive cybersecurity framework, orchestrating seamless system integration, and fostering a culture of innovation, we've achieved increase in operational efficiency and a  reduction in technology-related downtime. My strategic alignment of IT investments with business objectives has not only optimized our complex assay processes but also created a global support structure exemplifying security, reliability, and agility - critical pillars for our pioneering work in life sciences.</section>
+                        
                         <!-- Work experience container -->
-                        <div>
+                        <div class="mb-6">
+                            
                             <!-- Job history section 1 -->
                             <section></section>
+                            
                             <!-- Job history section 2 -->
                             <section></section>
+                            
                             <!-- Job history section 3 -->
                             <section></section>
+                            
                             <!-- Job history section 4 -->
                             <section></section>
+                        
                         </div>
+                        
                         <!-- Education section -->
-                        <section></section>
+                        <section class="mb-6"></section>
+                        
                         <!-- Certificates section -->
-                        <section></section>
+                        <section class="mb-6"></section>
+
                     </section>
+
                 </div>
             </article>
+            
+            <!-- Footer information -->
+            <footer></footer>
+
         </div>
         
 
