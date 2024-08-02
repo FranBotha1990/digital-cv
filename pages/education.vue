e9bdc54a18f76a3b482567e2ed7c959ae327c69a
FranBotha1990 <f.botha@outlook.com>
2024-08-02 12:15:30 +0300

    Create Base for 'projects.vue', Correct Spelling Mistakes, Update 'Summary.vue'

--- pages/education.vue
+++ pages/education.vue
@@ -1,179 +1,185 @@
 <template>
 	<div>
-		<!-- Certificate container -->
+		<!-- Certificate and education container -->
 		<div class="certificates text-center">
-		<h1>
-			<IconsEducationIcon class="pr-1 pl-2 [&>svg]:h-10 [&>svg]:w-10 text-custcol-teal" />Education
-		</h1>
+			
+			<!-- Education heading -->
+			<h1>
+				<IconsEducationIcon class="pr-1 pl-2 [&>svg]:h-10 [&>svg]:w-10 text-custcol-teal" />Education
+			</h1>
 
-		<div class="education-cnt">
-			<h2 class="pb-2 text-custcol-indigodye">North-West University</h2>
-			<p class="pb-1 text-sm italic">Bachelor of Science in Information Technology</p>
-			<p class="flex pb-2 text-sm text-custcol-indigodye"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>2021 - 2024 (In Progress, Final Semester)</p>
-			<section class="mb-4">
-				<h2 class="flex mb-2 border-b-2"><IconsTrophyIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-indigodye"/>Skills</h2>
-				<ul class="flex flex-wrap gap-0.5 justify-center">
-					<li v-for="skill in nwuSkills" key="skill" class="strengthItems">{{ skill }}</li>
-				</ul>
-			</section>
-		</div>
-
-		<div class="education-cnt">
-			<h2 class="pb-2 text-custcol-midnightgreen">North-West University</h2>
-			<p class="pb-1 text-sm italic">University Prepatory Programme: Bachelor of Science in Information Technology</p>
-			<p class="flex pb-2 text-sm text-custcol-midnightgreen"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>2020</p>
-			<section class="mb-4">
-				<h2 class="flex mb-2 border-b-2"><IconsTrophyIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-midnightgreen"/>Skills</h2>
-				<ul class="flex flex-wrap gap-0.5 justify-center">
-					<li v-for="skill in nwuPrepSkills" key="skill" class="majorSkillItems">{{ skill }}</li>
-				</ul>
-			</section>
-		</div>
-
-		<div class="education-cnt">
-			<h2 class="pb-2 text-custcol-skobeloff">CTI Education Group</h2>
-			<p class="pb-1 text-sm italic">Information Systems Engineering</p>
-			<p class="flex pb-2 text-sm text-custcol-skobeloff"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>2009</p>
-			<section class="mb-4">
-				<h2 class="flex mb-2 border-b-2"><IconsTrophyIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-skobeloff"/>Skills</h2>
-				<ul class="flex flex-wrap gap-0.5 justify-center">
-					<li v-for="skill in ctiSkills" key="skill" class="minorSkillItems">{{ skill }}</li>
-				</ul>
-			</section>
-		</div>
-		
-		<h1>
-			<IconsEducationIcon class="pr-1 pl-2 [&>svg]:h-10 [&>svg]:w-10 text-custcol-teal" />Certificates
-		</h1>
-		
-		<div class="flex flex-wrap gap-4 justify-center">
-			<!-- Certificates, repeated below -->
-			<div class="certificate-item">
-			<h2>GitHub Codespaces</h2>
-			<p>LinkedIn</p>
-			<p>July 2024</p>
-			<div class="image-container">
-				<img src="/assets/img/githubcodespace.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
+			<!-- Education section, repeated below -->
+			<div class="education-cnt">
+				<h2 class="pb-2 text-custcol-indigodye">North-West University</h2>
+				<p class="pb-1 text-sm italic">Bachelor of Science in Information Technology</p>
+				<p class="flex pb-2 text-sm text-custcol-indigodye"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>2021 - 2024 (In Progress, Final Semester)</p>
+				<section class="mb-4">
+					<h2 class="flex mb-2 border-b-2"><IconsTrophyIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-indigodye"/>Skills</h2>
+					<ul class="flex flex-wrap gap-0.5 justify-center">
+						<li v-for="skill in nwuSkills" key="skill" class="strengthItems">{{ skill }}</li>
+					</ul>
+				</section>
 			</div>
 
-			<div class="certificate-item">
-			<h2>HTML Essential Training</h2>
-			<p>LinkedIn</p>
-			<p>July 2024</p>
-			<div class="image-container">
-				<img src="/assets/img/html.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
-
-			<div class="certificate-item">
-			<h2>Learning GitHub Pages</h2>
-			<p>LinkedIn</p>
-			<p>July 2024</p>
-			<div class="image-container">
-				<img src="/assets/img/githubpages.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
+			<div class="education-cnt">
+				<h2 class="pb-2 text-custcol-midnightgreen">North-West University</h2>
+				<p class="pb-1 text-sm italic">University Prepatory Programme: Bachelor of Science in Information Technology</p>
+				<p class="flex pb-2 text-sm text-custcol-midnightgreen"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>2020</p>
+				<section class="mb-4">
+					<h2 class="flex mb-2 border-b-2"><IconsTrophyIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-midnightgreen"/>Skills</h2>
+					<ul class="flex flex-wrap gap-0.5 justify-center">
+						<li v-for="skill in nwuPrepSkills" key="skill" class="majorSkillItems">{{ skill }}</li>
+					</ul>
+				</section>
 			</div>
 
-			<div class="certificate-item">
-			<h2>Effective Listening</h2>
-			<p>LinkedIn</p>
-			<p>July 2023</p>
-			<div class="image-container">
-				<img src="/assets/img/effectivelistening.png" alt="Certificate" class="certificate-image rounded-md" />
+			<div class="education-cnt">
+				<h2 class="pb-2 text-custcol-skobeloff">CTI Education Group</h2>
+				<p class="pb-1 text-sm italic">Information Systems Engineering</p>
+				<p class="flex pb-2 text-sm text-custcol-skobeloff"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>2009</p>
+				<section class="mb-4">
+					<h2 class="flex mb-2 border-b-2"><IconsTrophyIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-skobeloff"/>Skills</h2>
+					<ul class="flex flex-wrap gap-0.5 justify-center">
+						<li v-for="skill in ctiSkills" key="skill" class="minorSkillItems">{{ skill }}</li>
+					</ul>
+				</section>
 			</div>
-			</div>
+			
+			<!-- Certificates heading -->
+			<h1>
+				<IconsEducationIcon class="pr-1 pl-2 [&>svg]:h-10 [&>svg]:w-10 text-custcol-teal" />Certificates
+			</h1>
+			
+				<div class="flex flex-wrap gap-4 justify-center">
+					
+					<!-- Certificates, repeated below -->
+					<div class="certificate-item">
+						<h2>GitHub Codespaces</h2>
+						<p>LinkedIn</p>
+						<p>July 2024</p>
+						<div class="image-container">
+							<img src="/assets/img/githubcodespace.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>How to be a Good Mentee and Mentor</h2>
-			<p>LinkedIn</p>
-			<p>July 2023</p>
-			<div class="image-container">
-				<img src="/assets/img/mentor.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>HTML Essential Training</h2>
+						<p>LinkedIn</p>
+						<p>July 2024</p>
+						<div class="image-container">
+							<img src="/assets/img/html.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>Microsoft Collaboration: SharePoint, Teams, Groups, and Yammer</h2>
-			<p>LinkedIn</p>
-			<p>July 2023</p>
-			<div class="image-container">
-				<img src="/assets/img/mscollaboration.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>Learning GitHub Pages</h2>
+						<p>LinkedIn</p>
+						<p>July 2024</p>
+						<div class="image-container">
+							<img src="/assets/img/githubpages.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>Developing Your Emotional Intelligence</h2>
-			<p>LinkedIn</p>
-			<p>June 2023</p>
-			<div class="image-container">
-				<img src="/assets/img/emotionalintelligence.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>Effective Listening</h2>
+						<p>LinkedIn</p>
+						<p>July 2023</p>
+						<div class="image-container">
+							<img src="/assets/img/effectivelistening.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>Complete GDPR, GDPR Certification, Data Protection, Privacy</h2>
-			<p>Udemy</p>
-			<p>January 2022</p>
-			<div class="image-container">
-				<img src="/assets/img/gdprcert.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>How to be a Good Mentee and Mentor</h2>
+						<p>LinkedIn</p>
+						<p>July 2023</p>
+						<div class="image-container">
+							<img src="/assets/img/mentor.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>Data Integrity</h2>
-			<p>Research Quality Association (RQA)</p>
-			<p>January 2022</p>
-			<div class="image-container">
-				<img src="/assets/img/dataintegrity.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>Microsoft Collaboration: SharePoint, Teams, Groups, and Yammer</h2>
+						<p>LinkedIn</p>
+						<p>July 2023</p>
+						<div class="image-container">
+							<img src="/assets/img/mscollaboration.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>GDPR Certification & Preparation to CIPP Certification</h2>
-			<p>Udemy</p>
-			<p>January 2022</p>
-			<div class="image-container">
-				<img src="/assets/img/gdpr.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>Developing Your Emotional Intelligence</h2>
+						<p>LinkedIn</p>
+						<p>June 2023</p>
+						<div class="image-container">
+							<img src="/assets/img/emotionalintelligence.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>Computerized Systems Validation to meet Quality System Requirements</h2>
-			<p>Quality First Academy</p>
-			<p>July 2019</p>
-			<div class="image-container">
-				<img src="/assets/img/csv.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>Complete GDPR, GDPR Certification, Data Protection, Privacy</h2>
+						<p>Udemy</p>
+						<p>January 2022</p>
+						<div class="image-container">
+							<img src="/assets/img/gdprcert.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>Microsoft: Certified Security Professional</h2>
-			<p>CQURE | Microsoft</p>
-			<p>May 2019</p>
-			<div class="image-container">
-				<img src="/assets/img/microsoftcsp.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>Data Integrity</h2>
+						<p>Research Quality Association (RQA)</p>
+						<p>January 2022</p>
+						<div class="image-container">
+							<img src="/assets/img/dataintegrity.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>Mimecast Certified Technology Specialist</h2>
-			<p>Mimecast</p>
-			<p>November 2015</p>
-			<div class="image-container">
-				<img src="/assets/img/goodclinicalpractice.png" alt="Certificate" class="certificate-image rounded-md" />
-			</div>
-			</div>
+					<div class="certificate-item">
+						<h2>GDPR Certification & Preparation to CIPP Certification</h2>
+						<p>Udemy</p>
+						<p>January 2022</p>
+						<div class="image-container">
+							<img src="/assets/img/gdpr.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
 
-			<div class="certificate-item">
-			<h2>Avast Security Expert</h2>
-			<p>Avast</p>
-			<p>December 2013</p>
-			<div class="image-container">
-				<img src="/assets/img/avast.png" alt="Certificate" class="certificate-image rounded-md" />
+					<div class="certificate-item">
+						<h2>Computerized Systems Validation to meet Quality System Requirements</h2>
+						<p>Quality First Academy</p>
+						<p>July 2019</p>
+						<div class="image-container">
+							<img src="/assets/img/csv.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
+
+					<div class="certificate-item">
+						<h2>Microsoft: Certified Security Professional</h2>
+						<p>CQURE | Microsoft</p>
+						<p>May 2019</p>
+						<div class="image-container">
+							<img src="/assets/img/microsoftcsp.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
+
+					<div class="certificate-item">
+						<h2>Mimecast Certified Technology Specialist</h2>
+						<p>Mimecast</p>
+						<p>November 2015</p>
+						<div class="image-container">
+							<img src="/assets/img/goodclinicalpractice.png" alt="Certificate" class="certificate-image rounded-md" />
+						</div>
+					</div>
+
+					<div class="certificate-item">
+						<h2>Avast Security Expert</h2>
+						<p>Avast</p>
+						<p>December 2013</p>
+						<div class="image-container">
+							<img src="/assets/img/avast.png" alt="Certificate" class="certificate-image rounded-md" />
+					</div>
+
+				</div>
 			</div>
-			</div>
-		</div>
 		</div>
 	</div>
 </template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,7 +1,52 @@
 <template>
-  <div>
-    
-  </div>
+	<div>
+		
+		<!-- Projects heading -->
+		<div class="certificates text-center">
+			<h1>
+				<IconsTrophyIcon class="pr-1 pl-2 [&>svg]:h-9 [&>svg]:w-9 text-custcol-teal" />My Projects
+			</h1>
+		</div>
+
+		<!-- Projects section -->
+		<article>
+			<div class="education-cnt">
+				<h2 class="pb-2 text-custcol-indigodye">Traffic Management DBMS</h2>
+				<p class="pb-1 text-sm italic">NWU University Group Project</p>
+				<p class="flex pb-2 text-sm text-custcol-indigodye"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>Designed in 2023</p>
+				<p class="pb-2">Traffic Management DBMS with ERD and functional database with test data.</p>
+				<section class="flex justify-center items-center">
+					<h2><a href="https://github.com/FranBotha1990/CMPG223_POS" class="flex" target="blank"><IconsGitHubIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-indigodye"/>GitHub Repository</a></h2>
+				</section>
+			</div>
+		</article>
+
+		
+		<article>
+			<div class="education-cnt">
+				<h2 class="pb-2 text-custcol-indigodye">Point of Sale System</h2>
+				<p class="pb-1 text-sm italic">NWU University Group Project</p>
+				<p class="flex pb-2 text-sm text-custcol-indigodye"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>Designed in 2022</p>
+				<p class="pb-2">POS system that manages inventory and sales for a business.</p>
+				<section class="flex justify-center items-center">
+					<h2><a href="https://github.com/FranBotha1990/CMPG223_POS" class="flex" target="blank"><IconsGitHubIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-indigodye"/>GitHub Repository</a></h2>
+				</section>
+			</div>
+		</article>
+
+		<article>
+			<div class="education-cnt">
+				<h2 class="pb-2 text-custcol-indigodye">C# Encryption Tool</h2>
+				<p class="pb-1 text-sm italic">NWU University Group Project</p>
+				<p class="flex pb-2 text-sm text-custcol-indigodye"><IconsDateIcon class="[&>svg]:h-4 [&>svg]:w-4 pr-1"/>Designed in 2022</p>
+					<p class="pb-2">An encryption tool that encrypts and decrypts files with a password entered by the user.</p>
+				<section class="flex justify-center items-center">
+					<h2><a href="https://github.com/ArminPretorius/EncryptionTool" class="flex" target="blank"><IconsGitHubIcon  class="pr-1 [&>svg]:h-7 [&>svg]:w-6 text-custcol-indigodye"/>GitHub Repository</a></h2>
+				</section>
+			</div>
+		</article>
+
+	</div>
 </template>
 
 <script setup>
